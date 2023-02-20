63b11b0a1933bb9c9ad0b73dc41f950c193118c9
Nilanjit Chakraborty <chakrabortynilanjit@gmail.com>
2023-02-20 13:53:34 +0530

    updated

--- stackNqueue.cpp
+++ stackNqueue.cpp
@@ -533,39 +533,36 @@ int main()
     return 0;
 }*/
 /*// Find the next Greater element
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-void printNGE(int arr[], int n)
-{
-    stack<int> s;
-    s.push(arr[0]);
-    for (int i = 1; i < n; i++)
-    {
-        if (s.empty())
-        {
-            s.push(arr[i]);
-            continue;
+class Solution {
+  public:
+    vector < int > nextGreaterElements(vector < int > & nums) {
+      int n = nums.size();
+      vector < int > nge(n, -1);
+      stack < int > st;
+      //2n for circular array , use n for non circular array
+      for (int i = 2 * n - 1; i >= 0; i--) {
+        while (!st.empty() && st.top() <= nums[i % n]) {
+          st.pop();
         }
-        while (s.empty() == false && s.top() < arr[i])
-        {
-            cout << s.top() << "--> " << arr[i] << endl;
-            s.pop();
-        }
-        s.push(arr[i]);
-    }
-    while (s.empty() == false)
-    {
-        cout << s.top() << "--> " << -1 << endl;
-        s.pop();
-    }
-}
 
-int main()
-{
-    int arr[] = {11, 13, 21, 3};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    printNGE(arr, n);
-    return 0;
+        if (i < n) {
+          if (!st.empty()) nge[i] = st.top();
+        }
+        st.push(nums[i % n]);
+      }
+      return nge;
+    }
+};
+int main() {
+  Solution obj;
+  vector < int > v {5,7,1,2,6,0};
+  vector < int > res = obj.nextGreaterElements(v);
+  cout << "The next greater elements are" << endl;
+  for (int i = 0; i < res.size(); i++) {
+    cout << res[i] << " ";
+  }
 }*/
 /*// The celebrity Problem
 #include <bits/stdc++.h>

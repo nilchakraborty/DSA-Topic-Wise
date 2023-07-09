94eb4de266e19db1470691dc08e9d3a9194340ab
Nilanjit Chakraborty <chakrabortynilanjit@gmail.com>
2023-07-09 13:31:04 +0530

    Trie Completed

--- Trie.cpp
+++ Trie.cpp
@@ -98,7 +98,7 @@ int main()
 		}
 	}
 }*/
-/*//Implement Trie – 2 (Prefix Tree)
+/*// Implement Trie – 2 (Prefix Tree)
 #include <bits/stdc++.h>
 using namespace std;
 struct Node {
@@ -721,3 +721,57 @@ int main()
 	findUniqueRows(M);
 	return 0;
 }*/
+/*// Number of Distinct Substrings in a String Using Trie
+#include<iostream>
+
+using namespace std;
+
+struct Node {
+  Node * links[26];
+
+  bool containsKey(char ch) {
+    return (links[ch - 'a'] != NULL);
+  }
+
+  Node * get(char ch) {
+    return links[ch - 'a'];
+  }
+
+  void put(char ch, Node * node) {
+    links[ch - 'a'] = node;
+  }
+};
+
+int countDistinctSubstrings(string & s) {
+  Node * root = new Node();
+  int count = 0;
+  int n = s.size();
+  for (int i = 0; i < n; i++) {
+    Node * node = root;
+
+    for (int j = i; j < n; j++) {
+      if (!node -> containsKey(s[j])) {
+        node -> put(s[j], new Node());
+        count++;
+      }
+      node = node -> get(s[j]);
+    }
+
+  }
+  return count + 1;
+}
+
+int main() {
+  string s1 = "ababa";
+  cout << "Total number of distinct substrings : " << countDistinctSubstrings(s1);
+  cout << "\n";
+
+  string s2 = "ccfdf";
+  cout << "Total number of distinct substrings : " << countDistinctSubstrings(s2);
+
+  return 0;
+}*/
+/*// Maximum XOR of Two Numbers in an Array
+https://takeuforward.org/data-structure/maximum-xor-of-two-numbers-in-an-array/*/
+/*// Maximum Xor Queries
+https://takeuforward.org/trie/maximum-xor-queries-trie/*/
